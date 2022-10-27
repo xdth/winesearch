e90ec4d9d2ba5037f5f1bdc2a14555df2fd22e7e
xdth <info@dthlabs.com>
2022-10-27 12:44:00 -0400

    modifying IWinesRepository

--- backend/src/repositories/IWinesRepository.ts
+++ backend/src/repositories/IWinesRepository.ts
@@ -1,7 +1,7 @@
 import { Wine } from "../entities/Wine";
 
 export interface IWinesRepository {
-  findAllWines(): Promise<Wine[]>;
-  findWineById(wine: number): Promise<Wine>;
+  findAll(): Promise<Wine[]>;
+  findById(wine: number): Promise<Wine>;
   save(wine: Wine): Promise<void>;
 }
