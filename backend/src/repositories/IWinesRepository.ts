187ddc5e41d73275dd003fcc8596c3d8152342de
xdth <info@dthlabs.com>
2022-10-27 12:43:07 -0400

    createWine use case

--- backend/src/repositories/IWinesRepository.ts
+++ backend/src/repositories/IWinesRepository.ts
@@ -3,4 +3,5 @@ import { Wine } from "../entities/Wine";
 export interface IWinesRepository {
   findAllWines(): Promise<Wine[]>;
   findWineById(wine: number): Promise<Wine>;
+  save(wine: Wine): Promise<void>;
 }
